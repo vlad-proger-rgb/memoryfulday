10fdc282fe399c7cc1ff1b16ad0f5495e8a9c153
vlad0 <vlad0307b@gmail.com>
2023-09-08 17:10:24 +0200

    The @PostMapping on /calendar/year/month/dayNumber is working correctly. But the image converts to null because I don't give such name parameter therefore it converts to null.

--- src/main/java/com/vladitproger/memoryfulday/controllers/CalendarController.java
+++ src/main/java/com/vladitproger/memoryfulday/controllers/CalendarController.java
@@ -8,10 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.Calendar;
